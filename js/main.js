a25b16b4fe4c79772da3431cccf8764ab2268f8d
kwoolery42 <kwoolery@liftablemedia.com>
2019-06-27 11:48:36 -0700

    lesson 22

---
+++ js/main.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: "#app",
+    data:{
+        state: true,
+        inputName: '',
+        names: [],
+        showError: false,
+        errorMessage: ''
+    },
+    methods:{
+        addNameToList(){
+           if(this.validate(this.inputName)){ 
+              this.names.push(this.inputName);
+              this.inputName = '';
+              this.showError = false;
+           }else{
+               this.errorMessage = 'You must enter a valid name';
+                this.showError = true;
+           }
+        },
+        validate(value){
+            if(value !== ''){
+                return true
+            }else{
+                return false
+            }
+        }
+    }
+})
